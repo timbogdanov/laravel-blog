53909578d96df5b8f6dff87c5b7c3da608c7a372
Tim Bogdanov <timbogdanov@Dmitriys-MBP.localdomain>
2021-07-26 16:39:55 -0700

    add file_path to profiles db

---
+++ database/migrations/2021_07_26_203022_add_profile_image_path_to_profiles.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProfileImagePathToProfiles extends Migration
+{
+    public function up()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->string('file_path');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('profiles', function (Blueprint $table) {
+            $table->dropColumn('file_path');
+        });
+    }
+}
